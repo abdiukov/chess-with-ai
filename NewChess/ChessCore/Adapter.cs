01b5939c8d1fd7cc227cc504c2c98234f8d947c0
a.abdiukov <a.abdiukov@gmail.com>
2021-03-27 19:07:33 +1100

    Finished working on Adapter

--- NewChess/ChessCore/Adapter.cs
+++ NewChess/ChessCore/Adapter.cs
@@ -45,209 +45,58 @@ namespace ChessCore
 
         private static string MakeEngineMove(Engine engine)
         {
-            DateTime start = DateTime.Now;
-
             engine.AiPonderMove();
 
             MoveContent lastMove = engine.GetMoveHistory().ToArray()[0];
 
-            string tmp = "";
+            string output;
 
             var sourceColumn = (byte)(lastMove.MovingPiecePrimary.SrcPosition % 8);
             var srcRow = (byte)(8 - (lastMove.MovingPiecePrimary.SrcPosition / 8));
             var destinationColumn = (byte)(lastMove.MovingPiecePrimary.DstPosition % 8);
             var destinationRow = (byte)(8 - (lastMove.MovingPiecePrimary.DstPosition / 8));
 
-            tmp += GetPgnMove(lastMove.MovingPiecePrimary.PieceType);
+            ConvertToMyCustomRows(ref srcRow);
+            ConvertToMyCustomRows(ref destinationRow);
 
-            if (lastMove.MovingPiecePrimary.PieceType == ChessPieceType.Knight)
-            {
-                tmp += GetColumnFromInt(sourceColumn + 1);
-                tmp += srcRow;
-            }
-            else if (lastMove.MovingPiecePrimary.PieceType == ChessPieceType.Rook)
-            {
-                tmp += GetColumnFromInt(sourceColumn + 1);
-                tmp += srcRow;
-            }
-            else if (lastMove.MovingPiecePrimary.PieceType == ChessPieceType.Pawn)
-            {
-                if (sourceColumn != destinationColumn)
-                {
-                    tmp += GetColumnFromInt(sourceColumn + 1);
-                }
-            }
 
-            if (lastMove.TakenPiece.PieceType != ChessPieceType.None)
-            {
-                tmp += "x";
-            }
+            output = sourceColumn + "-" + srcRow + " " + destinationColumn + "-" + destinationRow;
 
-            tmp += GetColumnFromInt(destinationColumn + 1);
-
-            tmp += destinationRow;
-
-            if (lastMove.PawnPromotedTo == ChessPieceType.Queen)
-            {
-                tmp += "=Q";
-            }
-            else if (lastMove.PawnPromotedTo == ChessPieceType.Rook)
-            {
-                tmp += "=R";
-            }
-            else if (lastMove.PawnPromotedTo == ChessPieceType.Knight)
-            {
-                tmp += "=K";
-            }
-            else if (lastMove.PawnPromotedTo == ChessPieceType.Bishop)
-            {
-                tmp += "=B";
-            }
-
-            DateTime end = DateTime.Now;
-
-            TimeSpan ts = end - start;
-
-            Console.Write(engine.PlyDepthReached + " ");
-
-            int score = engine.GetScore();
-
-            if (score > 0)
-            {
-                score = score / 10;
-            }
-
-            return tmp;
+            return output;
         }
 
-        public static string GetColumnFromInt(int column)
-        {
-            string returnColumnt;
 
-            switch (column)
+        //converting the Row so it is compatible with my program
+        //or in other words, converting the y value
+        private static void ConvertToMyCustomRows(ref byte RowToConvert)
+        {
+            switch (RowToConvert)
             {
                 case 1:
-                    returnColumnt = "a";
+                    RowToConvert = 7;
                     break;
                 case 2:
-                    returnColumnt = "b";
+                    RowToConvert = 6;
                     break;
                 case 3:
-                    returnColumnt = "c";
+                    RowToConvert = 5;
                     break;
                 case 4:
-                    returnColumnt = "d";
+                    RowToConvert = 4;
                     break;
                 case 5:
-                    returnColumnt = "e";
+                    RowToConvert = 3;
                     break;
                 case 6:
-                    returnColumnt = "f";
+                    RowToConvert = 2;
                     break;
                 case 7:
-                    returnColumnt = "g";
+                    RowToConvert = 1;
                     break;
                 case 8:
-                    returnColumnt = "h";
-                    break;
-                default:
-                    returnColumnt = "Unknown";
+                    RowToConvert = 0;
                     break;
             }
-
-            return returnColumnt;
-        }
-
-        private static string GetPgnMove(ChessPieceType pieceType)
-        {
-            string move = "";
-
-            if (pieceType == ChessPieceType.Bishop)
-            {
-                move += "B";
-            }
-            else if (pieceType == ChessPieceType.King)
-            {
-                move += "K";
-            }
-            else if (pieceType == ChessPieceType.Knight)
-            {
-                move += "N";
-            }
-            else if (pieceType == ChessPieceType.Queen)
-            {
-                move += "Q";
-            }
-            else if (pieceType == ChessPieceType.Rook)
-            {
-                move += "R";
-            }
-
-            return move;
-        }
-
-        private static byte GetRow(string move)
-        {
-            if (move != null)
-            {
-                if (move.Length == 2)
-                {
-                    return (byte)(8 - int.Parse(move.Substring(1, 1).ToLower()));
-                }
-            }
-
-            return 255;
-        }
-
-        private static byte GetColumn(string move)
-        {
-            if (move != null)
-            {
-                if (move.Length == 2)
-                {
-                    string col = move.Substring(0, 1).ToLower();
-
-                    switch (col)
-                    {
-                        case "a":
-                            {
-                                return 0;
-                            }
-                        case "b":
-                            {
-                                return 1;
-                            }
-                        case "c":
-                            {
-                                return 2;
-                            }
-                        case "d":
-                            {
-                                return 3;
-                            }
-                        case "e":
-                            {
-                                return 4;
-                            }
-                        case "f":
-                            {
-                                return 5;
-                            }
-                        case "g":
-                            {
-                                return 6;
-                            }
-                        case "h":
-                            {
-                                return 7;
-                            }
-                        default:
-                            return 255;
-                    }
-                }
-            }
-
-            return 255;
         }
 
     }
